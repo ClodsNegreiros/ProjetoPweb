501cf6daa19edf1697eb841a6c2928b52ff71cfb
João Marcos <joao.chaves@academico.ifpb.edu.br>
2023-12-05 10:11:33 -0300

    firestore instalacao

---
+++ src/firebase.config.ts
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBUFYoXZKkNn5z27_rp2nHhpqOGSxGsvNE",
+    authDomain: "gnosis-educacao.firebaseapp.com",
+    projectId: "gnosis-educacao",
+    storageBucket: "gnosis-educacao.appspot.com",
+    messagingSenderId: "603419257158",
+    appId: "1:603419257158:web:776336cf00a6a2d3295d47",
+    measurementId: "G-8EPPD2DFH6"
+  };
+  
