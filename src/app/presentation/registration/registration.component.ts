939a562456652afd0752da29e3bb89d16f73be72
ClodsNegreiros <clodsn@gmail.com>
2023-12-05 11:33:29 -0300

    feat: Melhorando lógica do resgistration

--- src/app/presentation/registration/registration.component.ts
+++ src/app/presentation/registration/registration.component.ts
@@ -1,21 +1,21 @@
-import { ActivatedRoute, Route, Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { UserService } from 'src/app/core/services/user.service';
-import { Student } from 'src/app/domain/entities/Student'
-import { Teacher} from 'src/app/domain/entities/Teacher'
-import { CustomEmailValidator } from 'src/app/core/validators/CustomEmailValidator';
+// import { UserService } from 'src/app/core/services/user.service';
+import { Student } from 'src/app/domain/entities/Student';
+import { Teacher } from 'src/app/domain/entities/Teacher';
+// import { CustomEmailValidator } from 'src/app/core/validators/CustomEmailValidator';
 import { TeacherService } from 'src/app/core/services/teacher.service';
 import { StudentService } from 'src/app/core/services/student.service';
+import { CustomPasswordValidator } from 'src/app/core/validators/CustomPasswordValidator';
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
-  styleUrls: ['./registration.component.css']
+  styleUrls: ['./registration.component.css'],
 })
 export class RegistrationComponent {
-
   atores: string[] = ['Professor', 'Aluno'];
 
   LoginForm: FormGroup;
@@ -26,22 +26,26 @@ export class RegistrationComponent {
   constructor(
     private _formBuilder: FormBuilder,
     private _teacherservice: TeacherService,
-    private _studentservice : StudentService,
+    private _studentservice: StudentService,
     private _snackBar: MatSnackBar,
     private _router: Router,
     private _route: ActivatedRoute
   ) {
-    this.LoginForm = this._formBuilder.group({
-      email: ['', [Validators.required, Validators.email]],
-      confirmEmail: ['', [Validators.required]],
-      password: ['', [Validators.required, Validators.minLength(6)]],
-      // confirmPassword: ['', [Validators.required, Validators.minLength(6)]],
-      ator: ['', Validators.required],
-    }, {
-      validators: [
-        CustomEmailValidator.sameEmail()
-      ]
-    });
+    this.LoginForm = this._formBuilder.group(
+      {
+        email: ['', [Validators.required, Validators.email]],
+        // confirmEmail: ['', [Validators.required]],
+        password: ['', [Validators.required, Validators.minLength(6)]],
+        // confirmPassword: ['', [Validators.required]],
+        ator: ['', Validators.required],
+      },
+      {
+        validators: [
+          // CustomEmailValidator.sameEmail(),
+          CustomPasswordValidator.samePassword(),
+        ],
+      }
+    );
   }
 
   ngOnInit(): void {}
@@ -55,26 +59,36 @@ export class RegistrationComponent {
   }
 
   async onSubmit(): Promise<void> {
+    try {
+      if (this.LoginForm.value.ator == 'professor') {
+        this.teacherRegistration();
+        return
+      }
+
+      this.studentRegistration();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async teacherRegistration(): Promise<void> {
     this.validateForm();
 
     if (this.LoginForm.invalid) {
       return;
     }
 
-    console.log('3')
-    const { email, password, ator } = this.LoginForm.value;
-  
-    const user= ator =="aluno" ? new Student({
-      email:email,
-      password:password
-    }) : new Teacher({
-      email:email,
-      password:password
+    const { email, password } = this.LoginForm.value;
+
+    const user = new Teacher({
+      email: email,
+      password: password,
     });
 
-    if(ator=='aluno'){
-      this._studentservice.addStudent(user as Student).subscribe((user:Student)=>{
-        
+    console.log(user);
+    this._teacherservice.addTeacher(user as Teacher).subscribe(
+      (user: Teacher) => {
+        console.log(user);
         this._snackBar.open(
           `o Usuário foi cadastrado com sucesso! Faça seu Login.`,
           'Ok',
@@ -96,35 +110,48 @@ export class RegistrationComponent {
             duration: 4000,
           }
         );
-      });
       }
-      else{
-        this._teacherservice.addTeacher(user as Teacher).subscribe((user:Teacher)=>{
-        
-          this._snackBar.open(
-            `o Usuário foi cadastrado com sucesso! Faça seu Login.`,
-            'Ok',
-            {
-              horizontalPosition: 'right',
-              verticalPosition: 'top',
-              duration: 4000,
-            }
-          );
-          this._router.navigate(['/login']);
-        },
-        (error) => {
-          this._snackBar.open(
-            `Erro ao cadastrar usuário. Por favor tente novamente.`,
-            'Ok',
-            {
-              horizontalPosition: 'right',
-              verticalPosition: 'top',
-              duration: 4000,
-            }
-          );
-        });
-      }
+    );
+  }
+
+  async studentRegistration(): Promise<void> {
+    this.validateForm();
+
+    if (this.LoginForm.invalid) {
+      return;
     }
 
+    const { email, password, ator } = this.LoginForm.value;
 
+    const user = new Student({
+      email: email,
+      password: password,
+    });
+
+    this._studentservice.addStudent(user as Student).subscribe(
+      (user: Student) => {
+        this._snackBar.open(
+          `o Usuário foi cadastrado com sucesso! Faça seu Login.`,
+          'Ok',
+          {
+            horizontalPosition: 'right',
+            verticalPosition: 'top',
+            duration: 4000,
+          }
+        );
+        this._router.navigate(['/login']);
+      },
+      (error) => {
+        this._snackBar.open(
+          `Erro ao cadastrar usuário. Por favor tente novamente.`,
+          'Ok',
+          {
+            horizontalPosition: 'right',
+            verticalPosition: 'top',
+            duration: 4000,
+          }
+        );
+      }
+    );
   }
+}
